dd72c18953ce008797c7c0431b42b5a451f9b78b
daizko <plastiakd@gmail.com>
2025-10-27 17:46:12 +0100

    added pin definitions for some peripherals

--- esp32-based-pda-code/src/main.cpp
+++ esp32-based-pda-code/src/main.cpp
@@ -15,6 +15,37 @@
 #include <GxEPD2_3C.h> //eink libraries
 #include <Fonts/FreeMonoBold9pt7b.h>
 
+struct Eink_Display
+{
+  int CS_PIN;
+  int BUSY_PIN;
+  int RES_PIN; // eink pins
+  int DC_PIN;
+};
+Eink_Display eInk = {10, 14, 46, 13};
+
+// 4.2'' EPD Module
+GxEPD2_BW<GxEPD2_420_GDEY042T81, GxEPD2_420_GDEY042T81::HEIGHT> display(GxEPD2_420_GDEY042T81(/*CS=5*/ eInk.CS_PIN, /*DC=*/eInk.DC_PIN, /*RES=*/eInk.RES_PIN, /*BUSY=*/eInk.BUSY_PIN));    // 400x300, SSD1683
+GxEPD2_3C<GxEPD2_420c_GDEY042Z98, GxEPD2_420c_GDEY042Z98::HEIGHT> display(GxEPD2_420c_GDEY042Z98(/*CS=5*/ eInk.CS_PIN, /*DC=*/eInk.DC_PIN, /*RES=*/eInk.RES_PIN, /*BUSY=*/eInk.BUSY_PIN)); // 400x300, SSD1683
+
+struct SD_Slot
+{
+  int CS_PIN;
+  int MOSI_PIN;
+  int MISO_PIN; // sdslot pins
+  int SCK_PIN;
+};
+SD_Slot sdSlot = {39, 35, 37, 36};
+
+#define led1 (1) // led pin
+
+struct rotaryEncoder
+{
+  int A;
+  int B;
+};
+rotaryEncoder rotE = {48, 47};
+
 // put function declarations here:
 int myFunction(int, int);
 
