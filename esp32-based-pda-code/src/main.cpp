971e7783f68db2c883d1f218570dca5cbf960456
daizko <plastiakd@gmail.com>
2025-11-04 09:16:26 +0100

    variable definition

--- esp32-based-pda-code/src/main.cpp
+++ esp32-based-pda-code/src/main.cpp
@@ -18,14 +18,14 @@
 
 struct Eink_Display
 {
-  int CS_PIN;
-  int BUSY_PIN;
-  int RES_PIN; // eink pins
-  int DC_PIN;
-  int CLK_PIN;
-  int MOSI_PIN;
+  const int CS_PIN = 39;
+  const int BUSY_PIN = 14;
+  const int RES_PIN = 40; // eink pins
+  const int DC_PIN = 37;
+  const int CLK_PIN = 36;
+  const int MOSI_PIN = 35;
 };
-Eink_Display eInk = {39, 14, 40, 37, 36, 35};
+Eink_Display eInk;
 
 // 4.2'' EPD Module
 GxEPD2_BW<GxEPD2_420_GDEY042T81, GxEPD2_420_GDEY042T81::HEIGHT> display(GxEPD2_420_GDEY042T81(/*CS=5*/ eInk.CS_PIN, /*DC=*/eInk.DC_PIN, /*RES=*/eInk.RES_PIN, /*BUSY=*/eInk.BUSY_PIN)); // 400x300, SSD1683
@@ -33,14 +33,12 @@ GxEPD2_BW<GxEPD2_420_GDEY042T81, GxEPD2_420_GDEY042T81::HEIGHT> display(GxEPD2_4
 
 struct SD_Slot
 {
-  int CS_PIN;
-  int MOSI_PIN;
-  int MISO_PIN; // sdslot pins
-  int SCK_PIN;
+  const int CS_PIN = 10;
+  const int MOSI_PIN = 11;
+  const int MISO_PIN = 13; // sdslot pins
+  const int SCK_PIN = 12;
 };
-SD_Slot sdSlot = {10, 11, 13, 12};
-
-#define led1 (1) // led pin
+SD_Slot sdSlot;
 
 #define SDA (8) // i2c pins
 #define SCL (9)
@@ -56,10 +54,19 @@ gpio_expander gpio_exp_2 = {0x41 >> 1, 17, 18, 41}; // not sure about what adres
 
 struct rotaryEncoder
 {
-  int A;
-  int B;
+  const int A = 4;
+  const int B = 5;
 };
-rotaryEncoder rotE = {4, 5};
+rotaryEncoder rotE;
+struct one_pin_devices
+{
+  int PIN;
+};
+one_pin_devices led1 = {1}; // case led
+one_pin_devices led2 = {6}; // error led
+one_pin_devices buzzer = {7};
+one_pin_devices vib_motor = {21};
+one_pin_devices battery_voltage = {3};
 
 SPIClass *fspi = NULL; // pointers to spi objects
 SPIClass *hspi = NULL;
@@ -70,7 +77,8 @@ void setup()
 {
   Serial.begin(115200);
 
-  pinMode(led1, OUTPUT); // led set as output
+  pinMode(led1.PIN, OUTPUT); // led set as output
+  pinMode(led2.PIN, OUTPUT);
 
   Wire.begin(SDA, SCL); // i2c inicialisation
 
@@ -89,6 +97,7 @@ void setup()
   if (!SD.begin(sdSlot.CS_PIN, *fspi))
   {
     Serial.println("Card Mount Failed");
+    digitalWrite(led2.PIN, HIGH); // error led
     return;
   }
   uint8_t cardType = SD.cardType();
