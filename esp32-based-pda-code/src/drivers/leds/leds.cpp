f39a2aef7246d59e3d234689c50343264b45bb9c
daizko <plastiakd@gmail.com>
2025-11-06 08:05:58 +0100

    created variables for led2 blinking

--- esp32-based-pda-code/src/drivers/leds/leds.cpp
+++ esp32-based-pda-code/src/drivers/leds/leds.cpp
@@ -6,27 +6,36 @@ namespace leds
     const int led1 = 1; // case led
     const int led2 = 6; // error led
 
-    static bool ledState = false;
-    static unsigned long int currentMillis;
-    static bool active1 = false;
-    static int remaining_blinks1;
-    static int length;
+    static bool ledState1 = false;
+    static bool ledState2 = false;
 
+    static unsigned long int currentMillis1;
+    static unsigned long int currentMillis2;
+
+    static bool active1 = false;
+    static bool active2 = false;
+
+    static int remaining_blinks1;
+    static int remaining_blinks2;
+
+    static int length1;
+    static int lenght2;
+    
     void begin()
     {
 
         pinMode(led1, OUTPUT); // led set as output
         pinMode(led2, OUTPUT);
-        currentMillis = 0;
+        currentMillis1 = 0;
     }
-    void start_blink_led1(int lenght, int times)
+    void start_blink_led1(int lenght1, int times)
     {
         remaining_blinks1 = times * 2; // bc on and off is one time
         if (times <= 0)
             return; // returns if minus number
-        ledState = false;
+        ledState1 = false;
         digitalWrite(led1, LOW);
-        currentMillis = millis();
+        currentMillis1 = millis();
         active1 = true;
     }
     void led1_blink()
@@ -39,11 +48,11 @@ namespace leds
             digitalWrite(led1, LOW);
             return;
         }
-        if (millis() - currentMillis >= length)
+        if (millis() - currentMillis1 >= length1)
         {
-            ledState = !ledState;
-            digitalWrite(led1, ledState ? HIGH : LOW);
-            currentMillis = millis();
+            ledState1 = !ledState1;
+            digitalWrite(led1, ledState1 ? HIGH : LOW);
+            currentMillis1 = millis();
             remaining_blinks1--;
             return;
         }
